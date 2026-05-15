1c70ae2955110d1923d5ddee7da6110ee8843629
haider786alpha <haiderhussain58n@gmail.com>
2026-05-15 16:41:57 +0500

    Update admin footer branding

--- admin-frontend/src/Layout/VerticalLayout/Footer.js
+++ admin-frontend/src/Layout/VerticalLayout/Footer.js
@@ -1,25 +1,24 @@
-import React from "react"
-import { Container, Row, Col } from "reactstrap"
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
 const Footer = () => {
   return (
-    <React.Fragment>
-      <footer className="footer">
-        <Container fluid ={true}>
-          <Row>
-            <Col sm={6}>{new Date().getFullYear()} © Upzet.</Col>
-            <Col sm={6}>
-              <div className="text-sm-end d-none d-sm-block">
-                Crafted with <i className="mdi mdi-heart text-danger"></i> by
-                Themesdesign
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    </React.Fragment>
+    <footer className="footer">
+      <Container fluid={true}>
+        <Row>
+          <Col sm={6}>
+            {new Date().getFullYear()} © DriveLedger.
+          </Col>
 
+          <Col sm={6}>
+            <div className="text-sm-end d-none d-sm-block">
+              DriveLedger Admin Panel
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
-}
+};
 
 export default Footer;
